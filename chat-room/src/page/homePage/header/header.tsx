eec2c2838401ffa5a9f4328154b5e8dd93a3e11c
Ivaz <ivazreza@gmail.com>
2025-04-04 15:21:30 +0700

    makes the header responsive with tailwind and colored

--- chat-room/src/page/homePage/header/header.tsx
+++ chat-room/src/page/homePage/header/header.tsx
@@ -3,7 +3,7 @@ import './header.scss';
 const Header = () => {
   return (
     <header className="headerHome flex justify-center items-center">
-      <h1 className="text-5xl py-7">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold py-7">
         Ivaz Chat App
       </h1>
     </header>
