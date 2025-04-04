9edee104024d217d0539d3c153bea299b41f49f1
Ivaz <ivazreza@gmail.com>
2025-04-04 15:03:30 +0700

    add header to homepage

---
+++ chat-room/src/page/homePage/header/header.tsx
@@ -0,0 +1,13 @@
+import './header.scss';
+
+const Header = () => {
+  return (
+    <header className="headerHome flex justify-center items-center">
+      <h1 className="text-5xl py-7">
+        Ivaz Chat App
+      </h1>
+    </header>
+  );
+}
+ 
+export default Header;
---
+++ chat-room/src/page/homePage/home/home.tsx
@@ -0,0 +1,12 @@
+import Header from '../header/header';
+import './home.scss';
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <Header />
+    </div>
+  );
+}
+ 
+export default HomePage;
