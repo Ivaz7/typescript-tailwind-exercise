ff08ba6655e2e71c7fb5e662c88c5263cca3118c
Ivaz <ivazreza@gmail.com>
2025-04-04 13:22:10 +0700

    learning how to use class

--- exercise/src/exerciseFundamental/typeTutorial.ts
+++ exercise/src/exerciseFundamental/typeTutorial.ts
@@ -64,5 +64,87 @@ const logModern = (age:number, name:string, report:number[], status:boolean, rel
   }
 }
 
+class UserImp implements Person {
+  name: string;
+  age: number;
+  report: number[];
+  status: boolean;
+  related: string[];
+
+  constructor(person: Person) {
+    this.name = person.name;
+    this.age = person.age;
+    this.report = person.report;
+    this.status = person.status;
+    this.related = person.related;
+  }
+
+  logClassImp() {
+    if (this.age === age) {
+      console.log("Age is same");
+    }
+  
+    if (this.name === name) {
+      console.log("Name is same");
+    }
+  
+    if (typeof this.report === typeof report) {
+      console.log("report is same");
+    }
+  
+    if (this.status === status) {
+      console.log("Status is same")
+    }
+  
+    if (typeof this.related === typeof related) {
+      console.log("Related is same")
+    }
+  }
+}
+
+class UserDefault {
+  name: string;
+  age: number;
+  status: boolean;
+  report: number[];
+  related: string[];
+
+  constructor(name: string, age: number, status: boolean, report: number[], related: string[]) {
+    this.name = name;
+    this.age = age;
+    this.status = status;
+    this.report = report;
+    this.related = related;
+  }
+
+  logClassDefault() {
+    if (this.age === age) {
+      console.log("Age is same");
+    }
+  
+    if (this.name === name) {
+      console.log("Name is same");
+    }
+  
+    if (typeof this.report === typeof report) {
+      console.log("report is same");
+    }
+  
+    if (this.status === status) {
+      console.log("Status is same")
+    }
+  
+    if (typeof this.related === typeof related) {
+      console.log("Related is same")
+    }
+  }
+}
+
+const user1 = new UserImp(user);
+const user2 = new UserDefault("John", 40, false, [1,2,3], ["Bob", "Mark"]);
+
 logClassic(age, name, report, status, related, user);
 logModern(age, name, report, status, related, user);
+
+user1.logClassImp();
+user2.logClassDefault();
