3801866b52ebde2416ed39fb892547dd74763e86
Ivaz <ivazreza@gmail.com>
2025-04-07 09:34:20 +0700

    makes header a lot better desgin

--- chat-room/src/page/homePage/header/header.tsx
+++ chat-room/src/page/homePage/header/header.tsx
@@ -2,10 +2,12 @@ import './header.scss';
 
 const Header = () => {
   return (
-    <header className="headerHome w-full flex justify-center items-center border-b shadow-xl/20 border-solid">
-      <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold py-7">
-        Ivaz Chat App
-      </h1>
+    <header className="headerHome w-full flex items-center border-b shadow-xl/20 border-solid">
+      <div className='d-flex md:m-4 lg:m-4 justify-betwwen'>
+        <h1 className="text-md sm:text-lg md:text-xl lg:text-2xl font-bold py-1 px-3 md:px-7 lg:px-10">
+          VazzChat
+        </h1>
+      </div>
     </header>
   );
 }
