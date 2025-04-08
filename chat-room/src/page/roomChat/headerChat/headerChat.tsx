755899a6d166575f31d091fda580a196c36e499a
Ivaz <ivazreza@gmail.com>
2025-04-08 11:52:59 +0700

    adding exit button for header

--- chat-room/src/page/roomChat/headerChat/headerChat.tsx
+++ chat-room/src/page/roomChat/headerChat/headerChat.tsx
@@ -1,13 +1,23 @@
-import { useAppSelector } from '../../../hooks/typedRedux';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../../hooks/typedRedux';
 import './headerChat.scss';
+import { setDeleteIdRoom, setDeleteUserName } from '../../../service/userData';
 
 const HeaderChat = () => {
   const userData = useAppSelector((state) => state.userDataSlice);
   const { idRoom, userName } = userData;
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const handleExit = () => {
+    dispatch(setDeleteIdRoom());
+    dispatch(setDeleteUserName());
+    navigate("/")
+  } 
 
   return (
-    <header className='headerChat w-full flex items-center border-b shadow-xl/20 border-solid'>
-      <div className='flex flex-col md:m-3 lg:m-4 md:px-7 lg:px-10 py-1 px-3'>
+    <header className='fixed top-0 left-0 flex flex-row justify-between headerChat w-full flex items-center border-b shadow-xl/20 border-solid md:px-7 lg:px-10 py-1 md:py-4 lg:py-5 px-3'>
+      <div className='flex flex-col'>
         <h1 className='text-md sm:text-lg md:text-xl lg:text-2xl font-bold'>
           {userName}
         </h1>
@@ -16,6 +26,16 @@ const HeaderChat = () => {
           {idRoom}
         </p>
       </div>
+
+      <button
+      style={{
+        backgroundColor: "var(--red)",
+      }}
+        className='py-1 md:py-2 md:px-3 px-4 rounded-sm sm:text-base md:text-lg lg:text-xl font-bold'
+        onClick={handleExit}
+      >
+        Exit
+      </button>
     </header>
   );
 }
